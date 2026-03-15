8630e9c4319aad25b7dde98cbba1205e0f83e987
Rafael Santos Ciriaco <santosrafael9279@gmail.com>
2026-03-15 13:20:39 -0600

    Finish the calculator OooooooUuuuuuuu YEAH!

--- script.js
+++ script.js
@@ -23,23 +23,20 @@ function operate(oper, n1, n2) {
     switch (oper){
         case "+":
             return add(n1,n2);
-            break;
         case "-":
-            return subtract(n1, n2);
-            break;
+            return subtract(n1, n2);       
         case "x":
-            return multiply(n1, n2);
-            break;
+            return multiply(n1, n2); 
         case "/":
-            return divide(n1, n2);
-            break;
+            return divide(n1, n2); 
     }
 }
 
 const buttonsNums = document.querySelectorAll(".btn-nums");
 const buttonsOperators = document.querySelectorAll(".operator");
 const screen = document.getElementById("screen");
-const btnProcces = document.getElementById("process")
+const btnProcces = document.getElementById("process");
+const btnClear = document.getElementById("clear");
 
 buttonsNums.forEach(btn => {
     btn.addEventListener("click", function(e) {
@@ -51,20 +48,29 @@ buttonsNums.forEach(btn => {
 buttonsOperators.forEach(btn => {
     btn.addEventListener("click", function(e) {
         const value = e.target.textContent;
+        if(operator !== ""){
+            num2 = Number(screen.textContent);
+            let result = operate(operator,num1,num2);
+            num1 = result;
+        }else{    
+            num1 = Number(screen.textContent);
+        }
         operator = value;
-        num1 = Number(screen.textContent);
         screen.textContent = "";
-
     })
 })
 
 btnProcces.addEventListener("click", function(){
     num2 = Number(screen.textContent);
-    screen.textContent = "";
-    console.log(operator);
-    console.log(num1);
-    console.log(num2);
     let result = operate(operator,num1,num2);
     screen.textContent = result;
+    num1 = result;
+    operator = "";
 })
 
+btnClear.addEventListener("click", function(){
+    screen.textContent = "";
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+})
